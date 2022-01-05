709ef3ec1d0d19b77ac490a7c5659a5280360370
rajneesh-hangingpanda <rajneesh.hangingpanda@gmail.com>
2022-01-05 12:45:47 +0530

    config url change

--- src/api/axios/config.js
+++ src/api/axios/config.js
@@ -1,5 +1,3 @@
 export default {
-   BASEURL: 'https://devrobos.com:5000/',
-  //BASEURL:'https://3.20.180.209:5000/',
- // BASEURL:'https://fa1f-223-177-185-181.ngrok.io/',
+   BASEURL: 'https://devrobos.com/',
 };
